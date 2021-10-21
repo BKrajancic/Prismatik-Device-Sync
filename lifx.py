0a6b7c2129008a3f3eb195318298f85907c98898
bkrajancic <benjamin.krajancic@student.uts.edu.au>
2021-10-21 21:57:52 +1100

    small update

--- lifx.py
+++ lifx.py
@@ -1,4 +1,5 @@
 import time
+import operator
 from utils import get_connection, _get_average_hsv
 from lifxlan import LifxLAN, Light
 
@@ -15,27 +16,16 @@ def _main():
 
     last_set = 0, 0, 0
     while True:
-        hue, saturation, value = _get_average_hsv(connection)
-
-        hue *= max_val 
-        hue = round(hue)
-
-        saturation *= max_val 
-        saturation = round(saturation)
-
-        value *= max_val
-        value = round(value)
-
-        current_set = hue, saturation, value
-        deltas = [
-            abs(lhs - rhs) > max_val * 0.2
-            for lhs, rhs in zip(last_set, current_set)
-        ]
-        if True in deltas:
+        current_set = _get_average_hsv(connection)
+        if any(map(0.05.__lt__, map(abs, map(operator.sub, last_set, current_set)))):
             last_set = current_set
-            bulb.set_color([hue, saturation, value, 8000], rapid=True)
+            current_set = [round(val * max_val) for val in current_set]
+            current_set.append(8000)
+            bulb.set_color(current_set, rapid=True)
 
-        time.sleep(0.01)
+            time.sleep(0.05)
+        else:
+            time.sleep(0.01)
 
 
 if __name__ == "__main__":
