62674181e864764896cd1e92f1c78b64aaab3e3d
bkrajancic <benjamin.krajancic@student.uts.edu.au>
2021-10-25 19:20:04 +1100

    Revert "small update"
    This reverts commit 0a6b7c2129008a3f3eb195318298f85907c98898.

--- lifx.py
+++ lifx.py
@@ -1,5 +1,4 @@
 import time
-import operator
 from utils import get_connection, _get_average_hsv
 from lifxlan import LifxLAN, Light
 
@@ -16,26 +15,27 @@ def _main():
 
     last_set = [0, 0, 0]
     while True:
-        current_set = _get_average_hsv(connection)
-        changes = list(map(0.05.__lt__, map(abs, map(operator.sub, last_set, current_set))))
-        if False not in changes:
-            current_set = [round(val * max_val) for val in current_set]
+        hue, saturation, value = _get_average_hsv(connection)
+
+        hue *= max_val 
+        hue = round(hue)
+
+        saturation *= max_val 
+        saturation = round(saturation)
+
+        value *= max_val
+        value = round(value)
+
+        current_set = hue, saturation, value
+        deltas = [
+            abs(lhs - rhs) > max_val * 0.2
+            for lhs, rhs in zip(last_set, current_set)
+        ]
+        if True in deltas:
             last_set = current_set
-            current_set.append(8000)
-            bulb.set_color(current_set, rapid=True)
-        else:
-            if changes[0]:
-                hue = round(current_set[0] * max_val)
-                bulb.set_hue(hue, rapid=True)
-                last_set[0] = hue
-            if changes[1]:
-                saturation = round(current_set[1] * max_val)
-                bulb.set_saturation(saturation, rapid=True)
-                last_set[1] = saturation
-            if changes[2]:
-                brightness = round(current_set[2] * max_val)
-                bulb.set_brightness(brightness, rapid=True)
-                last_set[2] = brightness
+            bulb.set_color([hue, saturation, value, 8000], rapid=True)
+
+        time.sleep(0.01)
 
 
 if __name__ == "__main__":
